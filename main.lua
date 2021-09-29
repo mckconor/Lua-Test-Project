827d8eaa17a632bd34334ee60f1d87d8f6c86aa7
Conor McKenna <mckennc9@tcd.ie>
2021-09-30 00:33:44 +0100

    Basic lessons

--- main.lua
+++ main.lua
@@ -1,5 +1,26 @@
-message = 12345
+message = 2
 
+function increaseMessage(i)
+    message = message + i
+end
+
+--This function double param value
+function double(val)
+    val = val * 2
+    return val
+end
+
+function getHalf(val)
+    local var = val
+    var = var / 2
+    return var
+end
+
+message = double(message)
+
+--[[
+    This function draws
+]]--
 function love.draw() 
     love.graphics.setFont(love.graphics.newFont(50))
     love.graphics.print(message)
